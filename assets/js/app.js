0ecf9e774b989fac724887b0dc604d8aa158dd43
uma <yadavuma419@gmail.com>
2023-02-24 08:44:25 +0530

    intial commit

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+import { modeBtn, shareBtn } from "./constants.js"
+
+modeBtn.addEventListener("click", () => {
+    const h = document.querySelector("html")
+    if (h.hasAttribute("data-bs-theme")) {
+        h.removeAttribute("data-bs-theme")
+        modeBtn.innerHTML = `<i class="bi bi-brightness-high"></i>`
+        modeBtn.classList.remove("btn-dark")
+        modeBtn.classList.add("btn-light")
+    } else {
+        h.setAttribute("data-bs-theme", "dark")
+        modeBtn.innerHTML = `<i class="bi bi-moon-stars-fill"></i>`
+        modeBtn.classList.remove("btn-light")
+        modeBtn.classList.add("btn-dark")
+    }
+})
+shareBtn.addEventListener("click", () => {
+    if (navigator.share) {
+        navigator.share({
+            title: "Awesome PWA Camera App",
+            text: "Switch,Capture,Share Moments",
+            url: "https://umagopinathyadav.github.io/PwA-Camera",
+        })
+    }
+})
